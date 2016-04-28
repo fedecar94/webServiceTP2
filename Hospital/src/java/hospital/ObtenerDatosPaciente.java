0c5cecd632f69b6d90822b0a6995b0c19b00fca3
fedecar94 <tekoreimante@outlook.com>
2016-04-28 16:21:08 -0400

    gson

--- Hospital/src/java/hospital/ObtenerDatosPaciente.java
+++ Hospital/src/java/hospital/ObtenerDatosPaciente.java
@@ -9,8 +9,7 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
-import hospital.Historial;
-
+import com.google.gson.Gson;
 /**
  *
  * @author tekor
@@ -41,7 +40,23 @@ public class ObtenerDatosPaciente {
      */
     @WebMethod(operationName = "historialFecha")
     public String historialFecha(@WebParam(name = "fecha1") Double fecha1, @WebParam(name = "fecha2") Double fecha2) {
-        Historial histo[] = new Historial[3];
-        return ;
+        Historial histo[] = new Historial[2];
+        histo[1].setId(123);
+        histo[1].setResponsable("asd");
+        histo[1].setDiagnosstico("asdasd");
+        histo[1].setEnfermedad("asdsad2");
+        histo[1].setFecha_hist(0);
+        histo[1].setHospital("macumbero");
+        histo[1].setSintomas("gripe");
+        
+        histo[0].setId(123);
+        histo[0].setResponsable("asd");
+        histo[0].setDiagnosstico("asdasd");
+        histo[0].setEnfermedad("asdsad2");
+        histo[0].setFecha_hist(0);
+        histo[0].setHospital("macumbero");
+        histo[0].setSintomas("gripe");
+        
+        return Gson.tojson(histo);
     }
 }
